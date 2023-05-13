8cc4eb0fba6043651a9c94c03d31e294d7ee1515
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-13 08:50:38 +0200

    Create index.js

---
+++ pages/superAdmin/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SidebarLayout from "../../components/SidebarLayout";
+
+const index = () => {
+  return <SidebarLayout></SidebarLayout>;
+};
+
+export default index;
